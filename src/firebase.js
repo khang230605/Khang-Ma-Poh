89db89daff5c7e005eeb78a845e25e5bf5715487
khang230605 <khangnguyennhat.work@gmail.com>
2025-12-17 00:51:57 +0700

    Main commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABb6HykpBFEymtkh4vXnj6H15hgIHP_Ug",
-  authDomain: "khang-ma-poh.firebaseapp.com",
-  projectId: "khang-ma-poh",
-  storageBucket: "khang-ma-poh.firebasestorage.app",
-  messagingSenderId: "562617429592",
-  appId: "1:562617429592:web:5275f5e458c01b8005242d",
-  measurementId: "G-62947NFYNB"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 
-// QUAN TRỌNG: Phải có chữ export ở đây
 export const db = getFirestore(app);
