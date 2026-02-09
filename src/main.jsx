ee3b4a0d74f3b4d29dfc539616868ecdb84a4504
khang230605 <khangnguyennhat.work@gmail.com>
2026-02-09 12:05:32 +0700

    Main.jsx fix helmet

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import { HelmetProvider } from 'react-helmet-async';
 
-root.render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </StrictMode>
 );
